864a2ca62dd53b4d41cf06185d0053e2822f009e
patrickstar <971602307@qq.com>
2024-08-12 15:52:48 +0800

    feat: 前台分类组件封装

--- src/components/XtxSwiper.vue
+++ src/components/XtxSwiper.vue
@@ -18,7 +18,7 @@ defineProps<{
 
 <template>
   <view class="carousel">
-    <swiper :circular="true" :autoplay="false" :interval="3000" @change="onChange">
+    <swiper :circular="true" :autoplay="true" :interval="3000" @change="onChange">
       <swiper-item v-for="item in list" :key="item.id">
         <navigator url="/pages/index/index" hover-class="none" class="navigator">
           <image mode="aspectFill" class="image" :src="item.imgUrl"></image>
