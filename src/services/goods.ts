71b6ade9fefbfbe1f2152d018f701c9b69a7ca1b
patrickstar <971602307@qq.com>
2024-08-13 16:22:34 +0800

    feat：商品详情的页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,10 +1,11 @@
+import type { GoodsResult } from '@/types/goods'
 import { http } from '@/utils/http'
 /**
  * 商品详情
  * @param id 商品id
  */
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: { id },
