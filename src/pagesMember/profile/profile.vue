21aa4023793a591568814f46a9428a094c20ff99
patrickstar <971602307@qq.com>
2024-08-14 20:55:27 +0800

    feat：个人中心-性别修改

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ProfileDetail } from '@/types/member'
+import type { ProfileDetail, Gender } from '@/types/member'
 import { getMemberProfileAPI } from '@/services/profile'
 import { putMemberProfileAPI } from '@/services/profile'
 import { useMemberStore } from '@/stores'
@@ -54,10 +54,16 @@ const onAvatarChange = () => {
   })
 }
 
+//修改性别
+const onGenderChange: UniHelper.RadioGroupOnChange = (ev) => {
+  profile.value.gender = ev.detail.value as Gender
+}
+
 // 点击保存提交表单
 const onSubmit = async () => {
   const res = await putMemberProfileAPI({
     nickname: profile.value?.nickname,
+    gender: profile.value.gender,
   })
   // 更新 Store 昵称
   memberStore.profile!.nickname = res.result.nickname
@@ -96,7 +102,7 @@ const onSubmit = async () => {
         </view>
         <view class="form-item">
           <text class="label">性别</text>
-          <radio-group>
+          <radio-group @change="onGenderChange">
             <label class="radio">
               <radio value="男" color="#27ba9b" :checked="profile?.gender === '男'" />
               男
