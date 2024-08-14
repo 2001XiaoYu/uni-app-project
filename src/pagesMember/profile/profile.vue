d69b0d5ebc8cdbd2e03692377dbc2c2e0d7fcf96
patrickstar <971602307@qq.com>
2024-08-14 18:52:38 +0800

    feat：个人信息-修改用户昵称

--- src/pagesMember/profile/profile.vue
+++ src/pagesMember/profile/profile.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import type { ProfileDetail } from '@/types/member'
 import { getMemberProfileAPI } from '@/services/profile'
+import { putMemberProfileAPI } from '@/services/profile'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
-// 获取个人信息
-const profile = ref<ProfileDetail>()
+// 获取个人信息, 修改个人信息需要提供初始值
+const profile = ref({} as ProfileDetail)
 const getMemberProfileData = async () => {
   const res = await getMemberProfileAPI()
   profile.value = res.result
@@ -40,12 +41,20 @@ const onAvatarChange = () => {
             profile.value!.avatar = avatar
             uni.showToast({ icon: 'success', title: '更新成功' })
           } else {
-            uni.showToast({ icon: 'error', title: '出现错误' })
+            uni.showToast({ icon: 'error', title: '接口错误' })
           }
         },
       })
     },
   })
+}
+
+// 点击保存提交表单
+const onSubmit = async () => {
+  const res = await putMemberProfileAPI({
+    nickname: profile.value?.nickname,
+  })
+  uni.showToast({ icon: 'success', title: '保存成功' })
 }
 </script>
 
@@ -73,7 +82,7 @@ const onAvatarChange = () => {
         </view>
         <view class="form-item">
           <text class="label">昵称</text>
-          <input class="input" type="text" placeholder="请填写昵称" :value="profile?.nickname" />
+          <input class="input" type="text" placeholder="请填写昵称" v-model="profile!.nickname" />
         </view>
         <view class="form-item">
           <text class="label">性别</text>
@@ -114,7 +123,7 @@ const onAvatarChange = () => {
         </view>
       </view>
       <!-- 提交按钮 -->
-      <button class="form-button">保 存</button>
+      <button @tap="onSubmit" class="form-button">保 存</button>
     </view>
   </view>
 </template>
