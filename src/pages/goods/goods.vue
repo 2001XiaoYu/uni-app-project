67a4429b8ca97ad350d186e2714c70cd56d3508e
patrickstar <971602307@qq.com>
2024-08-16 22:52:32 +0800

    feat：填写订单-提交订单

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -118,7 +118,7 @@ const onAddCart = async (ev: SkuPopupEvent) => {
 
 // 立即购买
 const onBuyNow = (ev: SkuPopupEvent) => {
-  uni.navigateTo({ url: `/pageOrder/create/create?skuId=$${ev._id}&count=${ev.buy_num}` })
+  uni.navigateTo({ url: `/pagesOrder/create/create?skuId=${ev._id}&count=${ev.buy_num}` })
 }
 </script>
 
